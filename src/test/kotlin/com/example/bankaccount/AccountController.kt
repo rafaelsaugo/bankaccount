1ace87926a6db84f5a1f807a21f38503c49cb0b8
Rafael Saugo <36832958+rafaelsaugo@users.noreply.github.com>
2020-05-16 00:11:29 -0300

    Add files via upload

---
+++ src/test/kotlin/com/example/bankaccount/AccountController.kt
@@ -0,0 +1,32 @@
+package com.example.bankaccount
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.lang.RuntimeException
+import javax.swing.text.AbstractDocument
+
+@RestController
+@RequestMapping ("accounts")
+class AccountController (val repository: AccountRepository) {
+
+    @PostMapping
+    fun create(@RequestBody account: Account) = ResponseEntity.ok(repository.save(account))
+
+    @GetMapping
+    fun read() = ResponseEntity.ok(repository.findAll())
+
+    @PutMapping("{document}")
+    fun update(@PathVariable document: String, @RequestBody account: Account): ResponseEntity<Account>{
+        val accountDBOptional = repository.findByDocument(document)
+        val toSave = accountDBOptional
+                .orElseThrow { RuntimeException("Account document: $document not found") }
+                .copy(name = account.name, balance = account.balance)
+        return ResponseEntity.ok(toSave)
+
+    @DeleteMapping("{document}")
+    fun delete(@PathVariable document: String) = repository
+            .findByDocument(document)
+            .ifPresent {repository.delete(it)}
+    }
+
+}
